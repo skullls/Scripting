f204c9b44e4ec0e2d2aa27437e8e102b0bcedefa
Vanesa Diosa <143772569+VanDiosa@users.noreply.github.com>
2024-09-25 10:45:00 -0500

    Creacion clase Subject Observer

--- OBSERVER/Program.cs
+++ OBSERVER/Program.cs
@@ -8,3 +8,15 @@ namespace Observador
     {
         void Actualizar(string mensaje);
     }
+
+     // Clase Subject
+    public class Subject
+    {
+        private List<IObserver> _observadores = new List<IObserver>();
+        private string _estado;
+
+        public void Suscribir(IObserver observador) => _observadores.Add(observador);
+        public void Desuscribir(IObserver observador) => _observadores.Remove(observador);
+
+    }
+
