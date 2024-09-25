fb6716bed3f73c5e6a0fc822ecb99a17aaad7feb
skullls <157409179+skullls@users.noreply.github.com>
2024-09-24 21:56:26 -0500

    Commit 2

---
+++ DIP/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace DIP
+{
+    // Interfaz INotificacion
+    public interface INotificacion
+    {
+        void Enviar(string mensaje);
+    }
+
+    // Clase concreta de CorreoElectronico
+    public class CorreoElectronico : INotificacion
+    {
+        public void Enviar(string mensaje)
+        {
+            Console.WriteLine($"Correo enviado: {mensaje}");
+        }
+    }
+
+    // Clase concreta de SMS
+    public class SMS : INotificacion
+    {
+        public void Enviar(string mensaje)
+        {
+            Console.WriteLine($"SMS enviado: {mensaje}");
+        }
+    }
+
+    // Clase ControladorDeNotificaciones (alto nivel)
+    public class ControladorDeNotificaciones
+    {
+        private INotificacion _notificacion;
+
+        public ControladorDeNotificaciones(INotificacion notificacion)
+        {
+            _notificacion = notificacion;
+        }
+
+        public void Notificar(string mensaje)
+        {
+            _notificacion.Enviar(mensaje);
+        }
+    }
+
+    // Clase principal
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INotificacion correo = new CorreoElectronico();
+            INotificacion sms = new SMS();
+
+            ControladorDeNotificaciones controlador = new ControladorDeNotificaciones(correo);
+            controlador.Notificar("Bienvenido por correo.");
+
+            controlador = new ControladorDeNotificaciones(sms);
+            controlador.Notificar("Bienvenido por SMS.");
+        }
+    }
+
+}
---
+++ LSP/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace LSP
+{
+    // Clase base Vehiculo
+    public class Vehiculo
+    {
+        public virtual void Avanzar()
+        {
+            Console.WriteLine("El vehículo está avanzando.");
+        }
+    }
+
+    // Clase derivada Coche que extiende Vehiculo
+    public class Coche : Vehiculo
+    {
+        public override void Avanzar()
+        {
+            Console.WriteLine("El coche está avanzando.");
+        }
+    }
+
+    // Clase derivada Bicicleta que extiende Vehiculo
+    public class Bicicleta : Vehiculo
+    {
+        public override void Avanzar()
+        {
+            Console.WriteLine("La bicicleta está avanzando.");
+        }
+    }
+
+    // Clase principal que usa el principio LSP
+    class Program
+    {
+        static void Main()
+        {
+            Vehiculo vehiculo1 = new Coche();
+            Vehiculo vehiculo2 = new Bicicleta();
+
+            MoverVehiculo(vehiculo1);  // El coche está avanzando.
+            MoverVehiculo(vehiculo2);  // La bicicleta está avanzando.
+        }
+
+        static void MoverVehiculo(Vehiculo vehiculo)
+        {
+            vehiculo.Avanzar();
+        }
+    }
+
+}
